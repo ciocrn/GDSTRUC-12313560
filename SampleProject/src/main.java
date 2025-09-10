bcecb23e3b62a6450f74c5e74e831fc32f7502be
Cio <ciomitchell.corona@benilde.edu.ph>
2025-09-10 09:19:57 +0800

    Added sample project

---
+++ SampleProject/src/main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class main {
+    public static  void  main(String[] args) {
+            System.out.print("Enter your name; ");
+            Scanner reader = new Scanner(System.in);
+            String name = reader.next();
+        System.out.println("Hello" + name + "!");
+    }
+}
